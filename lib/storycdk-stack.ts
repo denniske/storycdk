24d1d41c9972fa2f5415dac12200966c721b3e02
Dennis Keil <dennis.keil10@gmail.com>
2024-11-20 19:44:29 +0100

    astro-aws

--- lib/storycdk-stack.ts
+++ lib/storycdk-stack.ts
@@ -1,10 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {AstroAWS} from "@astro-aws/constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 
 export class StorycdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+
+    super(scope, id, props)
+
+    new AstroAWS(this, "AstroAWS", {
+      websiteDir: "../my-astro-project",
+    })
 
     // The code that defines your stack goes here
 
